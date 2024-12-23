3ac72a9fef2edc41402001367811626f54a05f71
Ezekiel A. Mitchell <ezekielam@icloud.com>
2024-12-22 23:03:38 -0800

    /ROS2/config/setup.sh -> fix issue where ros2_venv doesn't push to ~/.bashrc

---
+++ config/set.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Path to your virtual environment
+VENV_PATH="/home/test/ROS2/config/ros2_venv"
+
+# Activate the virtual environment
+source "$VENV_PATH/bin/activate"
+
+# Now you can run Python commands within the virtual environment
+python -m pip install --upgrade pip
+python setup.py
+
+# Deactivate the virtual environment when done
+# deactivate
